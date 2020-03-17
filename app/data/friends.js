22d25dc3a1bd1137e83964f356e6bcb97a4dae1a
Laura Faye <jim.laura2@tds.net>
2020-03-17 19:19:26 -0400

    modified html, js and api

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+        name: "Daniel",
+        photo: "https://www.irishtimes.com/polopoly_fs/1.3839287.1557430037!/image/image.jpg_gen/derivatives/box_620_330/image.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Alexander",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTg6njjl-TZWcBvJWH12tfoDKfkJ129UaGDv-zJq3q84U9LRx3A",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Sam",
+        photo: "https://cbsnews2.cbsistatic.com/hub/i/r/2010/04/16/6a954dc4-a642-11e2-a3f0-029118418759/thumbnail/640x654/d88b03a420f417dbbdd96bb93622afca/AP070202064419.gif",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Michael",
+        photo: "https://assets.mubicdn.net/images/cast_member/26906/image-original.jpg?1541026431",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Shaun",
+        photo: "https://m.media-amazon.com/images/M/MV5BMTI4NzAwNjM2MF5BMl5BanBnXkFtZTYwNzUwNDM2._V1_UY1200_CR83,0,630,1200_AL_.jpg",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Eddie",
+        photo: "https://townsquare.media/site/295/files/2019/09/Eddie-Money-crop.jpg?w=980&q=75",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
+];
+
+module.exports = friends;
